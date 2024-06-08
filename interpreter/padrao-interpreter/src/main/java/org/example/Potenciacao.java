2bbd2d27642fec2377d374f111ac53f6011a00d8
Pablo Barral <pablo.barral.kts@gmail.com>
2024-06-08 16:57:39 -0300

    interpreter

---
+++ interpreter/padrao-interpreter/src/main/java/org/example/Potenciacao.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Potenciacao implements InterpretadorExpressao {
+
+    private double x;
+    private double y;
+
+    public Potenciacao(Numero elementoEsquerda, Numero elementoDireita) {
+        x = elementoEsquerda.getNumero();
+        y = elementoDireita.getNumero();
+    }
+
+    public double interpretar() {
+        return Math.pow(x, y);
+    }
+}
